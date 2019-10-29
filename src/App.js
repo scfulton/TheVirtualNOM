6fb1ff5f724637f9438087d5fb1a4bcff711ce73
Scotty Fulton <sfulton@talentpath.com>
2019-10-29 09:58:02 -0500

    added nouislider, mongoose, create-react-app, and express to npm

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Team: N.A.M.E's</p>
+        <p>
+          The Virtual N.O.M
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
