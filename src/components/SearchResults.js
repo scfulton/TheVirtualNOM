6892202a4caa5b2f9b4b9b2c7566ad829f1c5a8b
Scotty <scotty.fulton01@gmail.com>
2019-11-10 16:38:46 -0600

    updated the client to fetch from heroku hosted api server... woot woot!

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -5,19 +5,32 @@ import '../styleSheets/SearchResults.css'
 class SearchResults extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { apiResponse: [] };
+    }
+
+    callAPI() {
+        fetch("http://thevirtualnomapiproduction.herokuapp.com/search")
+        // fetch("http://localhost:9000/search")
+        .then(res => res.text())
+        .then(res => this.setState({apiResponse: res}))
+    }
+    componentDidMount(){
+        this.callAPI()
     }
     render() { 
         return ( 
             <div className='resultContainer'>
-                <ol className='resultsList'>
+                <div> 
+                    {this.state.apiResponse}
+                </div>
+                {/* <ol className='resultsList'>
                     <li>Ordered</li>   
                     <li>List</li> 
                     <li>here</li> 
                     <li>of</li> 
                     <li>results</li> 
 
-                </ol>
+                </ol> */}
             </div>
          );
     }
