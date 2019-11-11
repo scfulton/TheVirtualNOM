bc327e368869f88dbac7ef44d749ee314dac8322
Scotty <scotty.fulton01@gmail.com>
2019-11-10 22:06:53 -0600

    lifted the state from result to window

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,26 +1,71 @@
-import React from 'react'
-import '../styleSheets/SearchResults.css'
-
+import React from "react";
+import "../styleSheets/SearchResults.css";
 
 class SearchResults extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div className='resultContainer'>
-                <ol className='resultsList'>
+  constructor(props) {
+    super(props);
+//     this.state = { 
+//         apiResponse: null,
+//         names: null,    
+//     };
+//   }
+
+//   callAPI() {
+//     fetch("https://thevirtualnomapiproduction.herokuapp.com/search")
+//       // fetch("http://localhost:9000/search")
+//       .then(res => res.text())
+//       //   .then(res => JSON.parse(res))
+//       .then(res => this.setState({ 
+//           apiResponse: res
+//      }));
+//   }
+//   componentDidMount() {
+//     this.callAPI();
+//   }
+//   componentWillUpdate(){
+//       this.callAPI()
+  }
+
+  render() {
+    // console.log("pre: " + this.state.apiResponse);
+    // const apiResponse = this.state.apiResponse;
+    // const jsonRes = JSON.parse(apiResponse);
+    // console.log("post: " + typeof jsonRes);
+
+    // if (this.state.apiResponse === null) {
+    //     console.log('in the if')
+    // } else {
+        // console.log('in the else')
+    //   const list = this.state.apiResponse === null ? {} : jsonRes.map(name => {
+    //     //   this.setState({names: name})
+
+    //     console.log(name.name)
+    //     return <li>{name.name}</li>;
+    //   });
+    // }
+
+    // const namesList = jsonRes.map(name => {
+    //     this.setState({names: name.name})
+    // })
+
+
+    return (
+      <div className="resultContainer">
+          <li>{this.props.children}</li>
+          {/* <ul>{list}</ul> */}
+        {/* <div>{this.state.apiResponse}</div> */}
+        {/* <ul>{this.state.names}</ul> */}
+        {/* <ol className='resultsList'>
                     <li>Ordered</li>   
                     <li>List</li> 
                     <li>here</li> 
                     <li>of</li> 
                     <li>results</li> 
 
-                </ol>
-            </div>
-         );
-    }
+                </ol> */}
+      </div>
+    );
+  }
 }
- 
+
 export default SearchResults;
--- src/components/SearchWindow.js
+++ src/components/SearchWindow.js
@@ -5,28 +5,76 @@ import Slider from "../components/DistanceSlider";
 import "../styleSheets/Slider.css";
 
 class SearchWindow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiResponse: null,
+      names: null
+    };
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	render() {
-		return (
-			<div>
-				<div>
-					<h1>The Virtual N.O.M</h1>
-					<div>
-						Enter your location:
-						<input></input>
-					</div>
-					<Slider></Slider>
+  callAPI() {
+    // fetch("https://thevirtualnomapiproduction.herokuapp.com/search")
+    fetch("http://localhost:9000/search")
+      .then(res => res.text())
+      //   .then(res => JSON.parse(res))
+      .then(res =>
+        this.setState({
+          apiResponse: res,
+          names: res.name
+        })
+      );
+  }
+  componentDidMount() {
+    this.callAPI();
+  }
+  render() {
+    console.log("pre: " + this.state.apiResponse);
+    const apiResponse = this.state.apiResponse;
+    // const jsonRes = JSON.parse(apiResponse);
+    // console.log("post: " + typeof jsonRes);
+    // const list =
+    //   this.state.apiResponse === null ? (
+    //     {}
+    //   ) : (
+    //     apiResponse.map(value => {
+    //       //   this.setState({names: name})
 
-					<button>Click me to search</button>
-					<SearchResults></SearchResults>
-				</div>
-			</div>
-		);
-	}
+    //       // console.log(name.name)
+    //       return <li>{value.value.name}</li>;
+    //     })
+    //   );
+
+    const rList = ({ apiResponse }) => {
+      if (!apiResponse) {
+        return null;
+      }
+      const listItems = apiResponse.value[0].map(item => (
+        <li> name: {item.name}</li>
+      ));
+      return (
+        <div>
+          <div> {listItems} </div>
+        </div>
+      );
+    };
+    return (
+      <div>
+        <div>
+          <h1>The Virtual N.O.M</h1>
+          <div>
+            Enter your location:
+            <input></input>
+          </div>
+          <Slider></Slider>
+
+          <button>Click me to search</button>
+          {/* <SearchResults>{this.state.apiResponse}</SearchResults> */}
+		  <SearchResults>{rList}</SearchResults>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SearchWindow;
