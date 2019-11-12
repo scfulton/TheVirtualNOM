ac42f6d3272ee722706b925b89370785e17bf19e
Ivan <ivantrincon@gmail.com>
2019-11-12 10:00:40 -0600

    set state for address, alert message for button

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,29 +1,46 @@
-import React from 'react'
-import '../styleSheets/SearchResults.css'
-
+import React from "react";
+import "../styleSheets/SearchResults.css";
 
 class SearchResults extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { apiResponse: [] };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: [] };
+  }
 
-    callAPI() {
-        fetch("https://thevirtualnomapiproduction.herokuapp.com/search")
-        // fetch("http://localhost:9000/search")
-        .then(res => res.text())
-        .then(res => this.setState({apiResponse: res}))
-    }
-    componentDidMount(){
-        this.callAPI()
-    }
-    render() { 
-        return ( 
-            <div className='resultContainer'>
-                <div> 
-                    {this.state.apiResponse}
-                </div>
-                {/* <ol className='resultsList'>
+  callAPI() {
+    fetch("https://thevirtualnomapiproduction.herokuapp.com/search")
+      // fetch("http://localhost:9000/search")
+      .then(res => {
+        return res.text();
+      })
+      .then(data => {
+        this.setState({ apiResponse: data });
+      });
+    //.then(res => this.setState({ apiResponse: res }));
+  }
+
+  myParse(a) {
+    let names = a.map(name => {
+      //console.log(name.value);
+      return (
+        <div>
+          <p> = {names.value.name} </p>
+        </div>
+      );
+    });
+  }
+
+  componentDidMount() {
+    this.callAPI();
+    this.myParse(this.state.apiResponse);
+  }
+
+  render() {
+    //this.myParse(this.state.apiResponse);
+    return (
+      <div className="resultContainer">
+        <div>{this.state.apiResponse}</div>
+        {/* <ol className='resultsList'>
                     <li>Ordered</li>   
                     <li>List</li> 
                     <li>here</li> 
@@ -31,9 +48,9 @@ class SearchResults extends React.Component {
                     <li>results</li> 
 
                 </ol> */}
-            </div>
-         );
-    }
+      </div>
+    );
+  }
 }
- 
+
 export default SearchResults;
