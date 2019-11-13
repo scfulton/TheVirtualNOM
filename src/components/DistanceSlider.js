1504c0ab782fff9446a8d8ba2ac6b1b4d3768eef
TrevorTaylor-TP <ttaylor@talentpath.com>
2019-11-13 10:52:08 -0600

    updated variables in slider

--- src/components/DistanceSlider.js
+++ src/components/DistanceSlider.js
@@ -2,49 +2,44 @@ import React from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 
-//const style = { width: 600, margin: 50 };
-const minMark = 1;
-const maxMark = 50;
+const min = 1;
+const max = 25;
+const step = 1;
 const marks = {
-	1: { label: `${minMark}` },
-	50: { label: `${maxMark}` }
+  1: { label: `${min}` },
+  25: { label: `${max}` }
 };
 class CustomizedSlider extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			min: 1,
-			max: 50,
-			step: 1,
-			value: 5
-		};
-	}
-	onSliderChange = value => {
-		console.log(value);
-		this.setState({
-			value
-		});
-	};
-	onAfterChange = value => {
-		console.log(value); //eslint-disable-line
-	};
-	render() {
-		return (
-			<div className="sliderContainer">
-				<Slider
-					value={this.state.value}
-					min={this.state.min}
-					max={this.state.max}
-					marks={marks}
-					step={null}
-					// min={this.state.min}
-					step={this.state.step}
-					onChange={this.onSliderChange}
-					onAfterChange={this.onAfterChange}
-				/>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 5
+    };
+  }
+  onSliderChange = value => {
+    console.log(value);
+    this.setState({
+      value
+    });
+  };
+  onAfterChange = value => {
+    console.log(value); //eslint-disable-line
+  };
+  render() {
+    return (
+      <div className="sliderContainer">
+        <Slider
+          value={this.state.value}
+          min={min}
+          max={max}
+          marks={marks}
+          step={step}
+          onChange={this.onSliderChange}
+          onAfterChange={this.onAfterChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default CustomizedSlider;
